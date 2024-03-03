450d6ff6d1c3726859f586c87eaefc9f9370419f
davis <davis.nguyenduy@gmail.com>
2024-03-04 00:23:54 +0700

    Initial commit

---
+++ src/Common.Core/Entities/Auditing/IFullAudited.cs
@@ -0,0 +1,5 @@
+namespace Common.Core.Entities.Auditing
+{
+    public interface IFullAudited : IAudited, IDeletionAudited
+    {}
+}
---
+++ src/Common.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+namespace Common.Core.Extensions
+{
+    /// <summary>
+    /// Extension methods for strings.
+    /// </summary>
+    public static class StringExtensions
+    {
+        public static bool IsNullOrWhiteSpace(this string? s)
+        {
+            return string.IsNullOrWhiteSpace(s);
+        }
+
+        public static bool IsNullOrEmpty(this string? s)
+        {
+            return string.IsNullOrEmpty(s);
+        }
+    }
+}
